7b510fda4f47b7eede0c76b5cc48ecc8f52fdf3a
segoja <segoja@gmail.com>
2021-11-23 21:48:01 +0100

    More updates

--- app/routes/secret.js
+++ app/routes/secret.js
@@ -2,11 +2,9 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class SecretRoute extends Route {
-  // do your secret model setup here
   @service session;
   
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
-  }
-  
+  }  
 }
